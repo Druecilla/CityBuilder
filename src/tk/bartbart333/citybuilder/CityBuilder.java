ccdcbf73138f0a8fda6407a913feded24b4dcc21
bartbart333 <bartholdbos@hotmail.com>
2014-09-06 13:47:53 +0200

    CityBuilder now holds an instance

--- src/tk/bartbart333/citybuilder/CityBuilder.java
+++ src/tk/bartbart333/citybuilder/CityBuilder.java
@@ -2,8 +2,19 @@ package tk.bartbart333.citybuilder;
 
 public class CityBuilder {
 	
+	private static CityBuilder instance;
+	
 	public CityBuilder(){
-		
+		instance = this;
+	}
+	
+	/**
+	 * @author Barthold
+	 * Returns the CityBuilder object.
+	 * @return CityBuilder object.
+	 */
+	public static CityBuilder getInstance(){
+		return instance;
 	}
 	
 	public static void main(String[] args){
