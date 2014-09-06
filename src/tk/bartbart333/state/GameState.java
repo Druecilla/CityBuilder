9a0c51b126fa53bccfaad18ab09caf754d98d746
bartbart333 <bartholdbos@hotmail.com>
2014-09-06 15:11:09 +0200

    GameState changed javadoc

--- src/tk/bartbart333/state/GameState.java
+++ src/tk/bartbart333/state/GameState.java
@@ -6,9 +6,21 @@ package tk.bartbart333.state;
  */
 public abstract class GameState {
 	
+	/**
+	 * @author Barthold
+	 * This method gets called when the state of the game gets set to the current GameState
+	 */
 	public abstract void init();
 	
+	/**
+	 * @author Barthold
+	 * This method gets called every frame to update the GameState
+	 */
 	public abstract void update();
 	
+	/**
+	 * @author Barthold
+	 * This method gets called every frame to render the GameState
+	 */
 	public abstract void render();
 }
