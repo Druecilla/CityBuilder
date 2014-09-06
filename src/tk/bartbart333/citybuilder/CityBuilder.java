5c687b201360a150ce504fd1ca485ccf51372e2d
bartbart333 <bartholdbos@hotmail.com>
2014-09-06 14:30:35 +0200

    CityBuilder now has clearScreen

--- src/tk/bartbart333/citybuilder/CityBuilder.java
+++ src/tk/bartbart333/citybuilder/CityBuilder.java
@@ -6,6 +6,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
+import org.newdawn.slick.opengl.TextureImpl;
 
 public class CityBuilder {
 	
@@ -19,6 +20,7 @@ public class CityBuilder {
 		initGL();
 		
 		while(!Display.isCloseRequested() && !Keyboard.isKeyDown(Keyboard.KEY_F4)){
+			clearScreen();
 			
 			Display.update();
 			Display.sync(60);
@@ -26,6 +28,13 @@ public class CityBuilder {
 		
 		Display.destroy();
 		System.exit(0);
+	}
+	
+	private void clearScreen(){
+		GL11.glLoadIdentity();
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		TextureImpl.bindNone();
+		GL11.glColor3f(1.0f, 1.0f, 1.0f);
 	}
 	
 	/**
