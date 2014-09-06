eeeb4e80bebc5afdaf41d4af462651e3157a8dfc
bartbart333 <bartholdbos@hotmail.com>
2014-09-06 14:12:30 +0200

    CityBuilder now has initGL

--- src/tk/bartbart333/citybuilder/CityBuilder.java
+++ src/tk/bartbart333/citybuilder/CityBuilder.java
@@ -1,13 +1,35 @@
 package tk.bartbart333.citybuilder;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class CityBuilder {
+	
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
 	
 	private static CityBuilder instance;
 	
 	public CityBuilder(){
 		instance = this;
+		initGL();
 	}
 	
+	/**
+	 * @author Barthold
+	 * Initializes the opengl display.
+	 */
+	private void initGL() {
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.setTitle("CityBuilder");
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * @author Barthold
 	 * Returns the CityBuilder object.
